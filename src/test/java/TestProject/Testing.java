a0ebe5b58d7b333b9afc667c0b137eb75aeb2e28
RajaRaji007 <raja98murugan@gmail.com>
2022-05-16 17:00:44 +0530

    Add Methods

---
+++ src/test/java/TestProject/Testing.java
@@ -0,0 +1,23 @@
+package TestProject;
+
+public class Testing {
+private void method1() {
+	System.out.println("check1");
+
+}
+private void method2() {
+	System.out.println("check2");
+
+}
+
+private void method3() {
+	System.out.println("check3");
+
+}
+
+private void method4() {
+	System.out.println("check4");
+
+}
+
+}
