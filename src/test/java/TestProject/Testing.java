c20b374dc2f03dcd1168ec28306cef7bad6c497a
RajaMuthaiya <rajamuthaiya98@gmail.com>
2022-05-16 18:35:43 +0530

    un code sari illa

--- src/test/java/TestProject/Testing.java
+++ src/test/java/TestProject/Testing.java
@@ -15,6 +15,14 @@ private void method3() {
 
 }
 
+private void method7() {
+	System.out.println("check7");
+
+}
+private void method8() {
+	System.out.println("check8");
+
+}
 private void method4() {
 	System.out.println("check4");
 
